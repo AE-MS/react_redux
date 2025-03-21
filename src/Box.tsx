dc3ddd55c171ec40f1e7aad8fa7ca6364935f895
AE ( ͡ಠ ʖ̯ ͡ಠ) <36546967+AE-MS@users.noreply.github.com>
2025-03-20 19:26:24 -0700

    State one

--- src/Box.tsx
+++ src/Box.tsx
@@ -7,12 +7,13 @@ export interface BoxProps {
 }
 
 export const Box: React.FC<BoxProps> = React.memo((props: BoxProps) => {
-  console.log("BOX RENDERED!");
-
+  console.log("Box component rendered");
   return (
     <div className="theBox">
       <h1>{props.name}</h1>
       <p>Age (from parent): {props.age}</p>
+      <p>Box Counter (from me) {/*???*/}</p>
+      <button onClick={/*???*/}>Count!</button>
     </div>
   );
 });
